b5b61d600a39d923bc0a3705e570408b765cb893
22RQAQ <1142556134@qq.com>
2024-05-26 16:15:14 +0800

    lab2-task2

--- app/src/main/java/com/example/aircarftwar2024/MainActivity.java
+++ app/src/main/java/com/example/aircarftwar2024/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aircarftwar2024;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this,OfflineActivity.class);
+        //传音乐开关
+        //intent.putExtra()
+
+        //在开始游戏按钮的onclik函数里调用
+        startActivity(intent);
     }
 }
