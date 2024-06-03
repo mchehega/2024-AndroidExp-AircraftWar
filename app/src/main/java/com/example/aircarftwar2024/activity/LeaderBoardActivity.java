419d648af25b3fc3093a028565c525e6ac40b9a0
lenovo <a@>
2024-06-03 19:12:07 +0800

    a

---
+++ app/src/main/java/com/example/aircarftwar2024/activity/LeaderBoardActivity.java
@@ -0,0 +1,19 @@
+package com.example.aircarftwar2024.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aircarftwar2024.R;
+
+public class LeaderBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+
+    }
+}
